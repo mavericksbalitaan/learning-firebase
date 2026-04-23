c760db7d5ba4441ab61435b4e195771906841347
Mavericks Balitaan <balitaanmavericks@gmail.com>
2026-04-23 14:40:11 +0800

    Add delete function

--- src/app/components/ShowAllDocuments.tsx
+++ src/app/components/ShowAllDocuments.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../lib/firebase"
 import { useEffect, useState } from "react";
 
@@ -24,14 +24,19 @@ export default function ShowAllDocuments() {
 		fetchUsers()
 	}, [setUsers])
 
+	const handleDelete = async (id) => {
+		await deleteDoc(doc(db, "test-collection", id))
+	}
+
 	return (
 		<>
-			<table className="text-center">
+			<table className="text-center border-separate border border-spacing-4">
 				<thead>
 					<tr>
 						<th className="underline min-w-[300px]">Doc ID</th>
 						<th className="underline min-w-[300px]">Name</th>
 						<th className="underline min-w-[300px]">Age</th>
+						<th className="underline min-w-[300px]">Option</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -40,6 +45,9 @@ export default function ShowAllDocuments() {
 							<td className="min-w-[300px]">{el.id}</td>
 							<td className="min-w-[300px]">{el.name}</td>
 							<td className="min-w-[300px]">{el.age}</td>
+							<td className="min-w-[300px]">
+								<button className="bg-red-500 text-white rounded-sm p-1 hover:cursor-pointer hover:text-black" onClick={() => handleDelete(el.id)}>Delete</button>
+							</td>
 						</tr>
 					))}
 				</tbody>
--- src/app/components/ShowAllDocumentsRT.tsx
+++ src/app/components/ShowAllDocumentsRT.tsx
@@ -19,14 +19,19 @@ export default function ShowAllDocumentsRT() {
 		return () => unsubscribe()
 	}, [])
 
+	const handleDelete = async (id) => {
+		await deleteDoc(doc(db, "test-collection", id))
+	}
+
 	return (
 		<>
-			<table className="text-center">
+			<table className="text-center border border-separate border-spacing-4">
 				<thead>
 					<tr>
 						<th className="underline min-w-[300px]">Doc ID</th>
 						<th className="underline min-w-[300px]">Name</th>
 						<th className="underline min-w-[300px]">Age</th>
+						<th className="underline min-w-[300px]">Option</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -35,6 +40,9 @@ export default function ShowAllDocumentsRT() {
 							<td className="min-w-[300px]">{el.id}</td>
 							<td className="min-w-[300px]">{el.name}</td>
 							<td className="min-w-[300px]">{el.age}</td>
+							<td className="min-w-[300px]">
+								<button className="bg-red-500 text-white rounded-sm p-1 hover:cursor-pointer hover:text-black" onClick={() => handleDelete(el.id)}>Delete</button>
+							</td>
 						</tr>
 					))}
 				</tbody>
